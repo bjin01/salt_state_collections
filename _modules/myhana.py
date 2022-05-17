ea252817ad583f1609d7dd49eec7aac1a1bd588a
bjin01 <jinbo01@hotmail.com>
2022-05-17 08:38:21 +0200

    myhana.py and start.sls

--- _modules/myhana.py
+++ _modules/myhana.py
@@ -3,14 +3,10 @@ import logging
 
 log = logging.getLogger(__name__)
 
-log.info("Here is Some Information")
-log.warning("You Should Not Do That")
-log.error("It Is Busted")
-
 def status():
     """
     A function to query SAP HANA information!
-    
+
     Create a pillar as below::
     myhana:
       hana-1.bo2go.home:
@@ -19,11 +15,18 @@ def status():
       hana-2.bo2go.home:
         sid: bjk
         instance: 00
-    
+
     CLI Example::
 
         salt '*' myhana.status
     """
-    remotehost =  __grains__["fqdn"] 
-    h = hana.HanaInstance('bjk', '00', '', remote_host=remotehost)
-    return "Installed: {} \nRunning: {} \nSR enabled: {} \nSR Status: {}.".format(h.is_installed(), h.is_running(), h.get_sr_state(), h.get_sr_status()['status'])
+    remotehost =  __grains__["fqdn"]
+    
+    sid =  __pillar__['saphana'][remotehost]['sid']
+    instno =  __pillar__['saphana'][remotehost]['instance']
+    log.info("%s %s %s"%(remotehost, sid, instno))
+    h = hana.HanaInstance(sid, instno, '', remote_host=remotehost)
+    try:
+        return "Installed: {} \nRunning: {} \nSR enabled: {} \nSR Status: {}.".format(h.is_installed(), h.is_running(), h.get_sr_state(), h.get_sr_status()['status'])
+    except:
+        return "Error, it seems HANA is not installed or mounted at all."
